5f83d06da5421711b3b0278f57740628fbe7ca5f
Alok Chauhan <alokc0365@gmail.com>
2025-06-24 02:51:04 +0530

    Refactor Vercel configuration and server setup; add .vercel to .gitignore

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import { connect } from "mongoose";
 import connectDB from "./configs/mongodb.js";
+import serverless from "serverless-http";
 
-// App Config
-const PORT = process.env.PORT || 4000;
 const app = express();
-await connectDB();
-// API Routes
-app.get("/", (req, res) => {
-  res.send("API Working");
-});
 
-// Initialize Middleware
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Connect to DB
+await connectDB();
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("API Working from Vercel!");
 });
+
+// ❗ Export handler for Vercel
+export const handler = serverless(app);
